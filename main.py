785982ffd03caca7fceda6444feeb10dd274f64b
aliceliu <aliceworkmail@gmail.com>
2012-12-02 09:29:47 -0800

    home works

--- main.py
+++ main.py
@@ -23,7 +23,16 @@ from randomkaraoke import *
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+
 class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        choice = self.request.get("choice")
+        template_values = {}
+        template = jinja_environment.get_template("home.html")
+        self.response.out.write(template.render(template_values))
+
+
+class KaraokeHandler(webapp2.RequestHandler):
     def get(self):
         choice = self.request.get("choice")
         song = get_song(choice)
@@ -39,12 +48,7 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template("karaoke.html")
         self.response.out.write(template.render(template_values))
         
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        choice = self.request.get("choice")
-        template_values = {}
-        template = jinja_environment.get_template("home.html")
-        self.response.out.write(template.render(template_values))
+
         
       
 
